f00a3e4da0baac902a0ddfa132afc856cf052fc1
CeliaGC <garciacastillacelia@gmail.com>
2023-10-25 10:24:00 +0200

    add-cloudinary-drag-drop

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -5,11 +5,14 @@ import ImageHandler from '../handler/ImageHandler';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+
 
 
 
 function UpLoadForm() {
-  const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
+
+    const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
 
   const handleImageChange = (event) => {
     const picture = event.target.input;
@@ -35,68 +38,90 @@ function UpLoadForm() {
 
 }
 
-  return (
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget({
+      cloudName:'dvx5np4ma',
+      uploadPreset:'m2bwuxr6'
+    }, function (error, result){
+      console.log(result.info.secure_url)
+    });
+  
+  }, [])
 
+
+   return(
     <>
-     <Form style={{borderRadius:"0.625rem", border:"2px solid #d63384", height:"80%", width:"80%", marginTop:"10%", marginLeft:"10%", backgroundColor:"rgba(153, 153, 153, 0.7)"}} onSubmit={handleSubmit(onSubmit)}>
-      <h2 style={{backgroundColor:"rgba(153, 153, 153, 1)", borderRadius:"0.625rem", border:"2px solid #d63384"}}>New Image</h2>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label htmlFor="id"></Form.Label>
-            <Form.Control type="hidden"/>
-          </Form.Group>
-          <p>Make sure you fill up every field</p>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label htmlFor="imageName">Title</Form.Label>
-            <Form.Control name='imageName' style={{backgroundColor:"rgba(255, 233, 246, 1)",marginLeft:"10%", width:"80%"}} {...register("imageName", { required: true })} />
-         {errors.terms && <span>Debe rellenar este campo</span>}
-          </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label htmlFor="category">Category</Form.Label>
-                        <select style={{borderRadius:"0.0625rem", backgroundColor:"rgba(255, 233, 246, 1)"}}{...register("category", { required: true })} name="category" >
-                          <option value="nature">Nature</option>
-                          <option value="society">Society</option>
-                          <option value="science">Science</option>
-                        </select>
-                        {errors.terms && <span>Debe rellenar este campo</span>}
-          </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label htmlFor="ImageSource">Image Source</Form.Label>
-            <Form.Control style={{backgroundColor:"rgba(255, 233, 246, 1)", width:"80%",marginLeft:"10%"}} {...register("ImageSource", { required: true })} name="ImageSource" placeholder='URL' onChange={handleImageChange}/>
-            {errors.terms && <span>Debe rellenar este campo</span>}
-          </Form.Group>
-    
-          <Button style={{borderRadius:"0.625rem", border:"2px solid #d63384"}} variant="secondary" type="submit" onClick={handleAddClick}>
-            Submit
-          </Button>
-    
-          <Link to="/gallery" >
-          <Button variant='secondary' bg='dark' id="back" type="button" value="Back to Gallery" style={{borderRadius:"0.625rem", border:"2px solid #d63384"}}>
-            Back to Gallery
-          </Button>
-          </Link>
-    
-          <Alert show={showAlert} variant="success" onClose={handleAlertClose} dismissible>
-                    <Alert.Heading>Image added to you collections</Alert.Heading>
-                     <p>
-                       Enjoy your gallery!
-                     </p>
-                     <hr />
-                     <div className="d-flex justify-content-end">
-                       <Button onClick={handleAlertClose} variant="outline-success">
-                         Close
-                       </Button>
-                     </div>
-                     </Alert>
-                </Form>
-                
-          </> 
+    <Form style={{borderRadius:"0.625rem", border:"2px solid #d63384", height:"80%", width:"80%", marginTop:"10%", marginLeft:"10%", backgroundColor:"rgba(153, 153, 153, 0.7)"}} onSubmit={handleSubmit(onSubmit)}>
+    <h2 style={{backgroundColor:"rgba(153, 153, 153, 1)", borderRadius:"0.625rem", border:"2px solid #d63384"}}>New Image</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label htmlFor="id"></Form.Label>
+          <Form.Control type="hidden"/>
+        </Form.Group>
+        <p>Make sure you fill up every field</p>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label htmlFor="imageName">Title</Form.Label>
+          <Form.Control name='imageName' style={{backgroundColor:"rgba(255, 233, 246, 1)",marginLeft:"10%", width:"80%"}} {...register("imageName", { required: true })} />
+       {errors.terms && <span>Debe rellenar este campo</span>}
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label htmlFor="category">Category</Form.Label>
+                      <select style={{borderRadius:"0.0625rem", backgroundColor:"rgba(255, 233, 246, 1)"}}{...register("category", { required: true })} name="category" >
+                        <option value="nature">Nature</option>
+                        <option value="society">Society</option>
+                        <option value="science">Science</option>
+                      </select>
+                      {errors.terms && <span>Debe rellenar este campo</span>}
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label htmlFor="ImageSource">Image Source</Form.Label>
+          <Form.Control style={{backgroundColor:"rgba(255, 233, 246, 1)", width:"80%",marginLeft:"10%"}} {...register("ImageSource", { required: true })} name="ImageSource" placeholder='URL' onChange={handleImageChange}/>
+          {errors.terms && <span>Debe rellenar este campo</span>}
+        </Form.Group>
+  
+        <Button style={{borderRadius:"0.625rem", border:"2px solid #d63384"}} variant="secondary" type="submit" onClick={handleAddClick}>
+          Submit
+        </Button>
+  
+        <Link to="/gallery" >
+        <Button variant='secondary' bg='dark' id="back" type="button" value="Back to Gallery" style={{borderRadius:"0.625rem", border:"2px solid #d63384"}}>
+          Back to Gallery
+        </Button>
+        </Link>
+  
+        <Alert show={showAlert} variant="success" onClose={handleAlertClose} dismissible>
+                  <Alert.Heading>Image added to you collections</Alert.Heading>
+                   <p>
+                     Enjoy your gallery!
+                   </p>
+                   <hr />
+                   <div className="d-flex justify-content-end">
+                     <Button onClick={handleAlertClose} variant="outline-success">
+                       Close
+                     </Button>
+                   </div>
+                   </Alert>
+              </Form> 
+              <button onClick={()=> widgetRef.current.open()}>Upload</button>
 
-     
-  )
-
+        </> 
+  
+    
+   )
 }
+
+
+
+
+
+
+
+
+
 
 export default UpLoadForm
     
