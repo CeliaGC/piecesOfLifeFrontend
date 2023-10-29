aea248242eac7a4df7d8fdbf0f8e7a5d03f44d09
CeliaGC <garciacastillacelia@gmail.com>
2023-10-29 22:53:46 +0100

    add-logo-and-favicon

--- src/components/myNavbar.jsx
+++ src/components/myNavbar.jsx
@@ -13,10 +13,12 @@ function MyNavbar() {
 
     <Navbar style={{borderRadius:"0.625rem", border:"2px solid #d63384"}} bg='secondary' variant='dark' expand="lg" fixed="top" >
       <Container>
+        <img width={'50rem'} height={'45rem'} src="src\assets\images\pieces_logo.png" alt="camera-logo" />
         <Navbar.Brand href="http://localhost:5173/">Pieces of life</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+          
           <Nav.Link href="http://localhost:5173/">Home</Nav.Link>
           <Nav.Link href="http://localhost:5173/gallery">Gallery</Nav.Link>
           <Nav.Link href="http://localhost:5173/uploadImage">Add Image</Nav.Link>
